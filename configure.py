4c6c7207d5cb8e93c2ab4ee4b9af08d8ae6aa3d4
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-07-04 10:58:54 -0700

    update tools

--- configure.py
+++ configure.py
@@ -159,8 +159,8 @@ if not config.non_matching:
 # Tool versions
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20260305"
-config.dtk_tag = "v0.3.2"
-config.objdiff_tag = "v3.7.0"
+config.dtk_tag = "v0.3.5"
+config.objdiff_tag = "v3.7.1"
 config.sjiswrap_tag = "v1.2.1"
 config.wibo_tag = "1.0.0"
 
